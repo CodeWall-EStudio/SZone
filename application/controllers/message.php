534555b5edbf3db7c3b1f452fcca9a97394c15c4
Code Wall-E <codewalle@gmail.com>
2013-12-04 21:55:27 +0800

    df

---
+++ application/controllers/message.php
@@ -0,0 +1,39 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * SZone Application Controller Class
+ *
+ * This class object extends from the CodeIgniter super class CI_Controller.
+ *
+ * @package		SZone
+ * @subpackage	application
+ * @category	controllers
+ * @author		Code Wall-E Studio
+ * @link		http://codewalle.com
+ */
+class Folder extends SZone_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $err = '';
+        directory_acquire(
+            $this->config->item('upload-path'),
+            md5($this->config->item('upload-path')),
+            $this->config->item('dir-file-num'),
+            $err
+        );
+
+        $this->load->model('User_model');
+        $query = $this->User_model->get_where();
+        //var_dump($query->results());
+
+    }
+}
+// END Controller class
+
+/* End of Me Controller.php */
+/* Location: ./application/controllers/folder.php */
