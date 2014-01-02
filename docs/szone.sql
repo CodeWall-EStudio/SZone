450b8212d78e50d36a3062060024894668e63de4
Code Wall-E <codewalle@gmail.com>
2014-01-03 02:42:54 +0800

    sql

--- docs/szone.sql
+++ docs/szone.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `files` (
   `type` tinyint(2) unsigned zerofill DEFAULT NULL COMMENT '文件类型',
   `mimes` varchar(60) NOT NULL,
   `ref` int(8) NOT NULL DEFAULT '1' COMMENT '文件引用计数',
-  `convert` INT(2) UNSIGNED ZEROFILL NOT NULL DEFAULT '0',
+  `create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `md5` (`md5`),
   UNIQUE KEY `path` (`path`)
