9a39994dfdfd003706c391e36f2a1aaf392abccb
Code Wall-E <codewalle@gmail.com>
2014-01-05 00:56:44 +0800

    add skey support

--- application/core/SZone_Controller.php
+++ application/core/SZone_Controller.php
@@ -13,7 +13,7 @@
 class SZone_Controller extends CI_Controller {
 
     protected $user = array();
-    protected $encodeKey = '';
+    protected $skey = '';
     protected $grouplist = array();
     protected $deplist = array();
     protected $depinfolist = array();
@@ -28,7 +28,20 @@ class SZone_Controller extends CI_Controller {
 
     protected function set_user()
     {
-        $encodeKey = $this->input->cookie('encode_key');
+        $this->skey = $this->input->get_post('skey', TRUE);
+        if (!$this->skey) {
+            $this->skey = $this->input->cookie('skey');
+        }
+        if (!$this->skey) {
+            $this->cas();
+        } else {
+            $user_data = $this->check_skey($this->skey);
+            $this->verify_user($user_data);
+        }
+    }
+
+    protected function cas()
+    {
         // CAS Check
         $this->load->library('phpCAS');
         phpCAS::setDebug();
@@ -39,82 +52,90 @@ class SZone_Controller extends CI_Controller {
             $user_str = phpCAS::getUser();
             $user_data = json_decode($user_str);
             if (is_null($user_data)) {
-                log_message('error', 'CAS 登录返回字符串解析错误：'.$user_str);
-                if ($this->uri->uri_string() === '') {
-                    show_error('登录服务器不可用，请联系系统管理员', 503);
-                } else {
-                    $this->json_error('登录服务器不可用，请联系系统管理员', 503);
-                }
+                log_message('ERROR', 'CAS 登录返回字符串解析错误：'.$user_str);
+                $this->show_error('登录服务器不可用，请联系系统管理员', 503);
             }
             $this->user['name'] = $user_data->loginName;
-            $this->encodeKey = $user_data->encodeKey;
-            log_message('info', '用户'.$this->user['name'].'登录：'.$this->encodeKey);
+            $this->skey = $user_data->encodeKey;
+            log_message('DEBUG', '用户'.$this->user['name'].'登录：'.$this->skey);
 
+            $user_data = $this->check_skey($this->skey);
+            $this->verify_user($user_data);
 
-            // 使用encodeKey获取用户信息
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'http://mapp.71xiaoxue.com/components/getUserInfo.htm');
-            curl_setopt($ch, CURLOPT_HEADER, 1);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, 'encodeKey='.$this->encodeKey);
-            $user_str = curl_exec($ch);
-            curl_close($ch);
-
-            $user_data = json_decode(strstr($user_str, '{'));
-            if (is_null($user_data)) {
-                log_message('error', '获取用户信息返回字符串解析错误：'.$user_str);
-                if ($this->uri->uri_string() === '') {
-                    show_error('用户数据服务器不可用，请联系系统管理员', 503);
-                } else {
-                    $this->json_error('用户数据服务器不可用，请联系系统管理员', 503);
-                }
-            }
-
-            if ($user_data->resultMsg != 'ok' || !$user_data->success) {
-                log_message('error', '用户信息无法正确验证：'.$user_str);
-                if ($this->uri->uri_string() === '') {
-                    show_error('无法验证用户信息，请联系系统管理员', 403);
-                } else {
-                    $this->json_error('无法验证用户信息，请联系系统管理员', 403);
-                }
-            }
-
-            // 检查用户是否已经登录
-            $this->load->model('User_model');
-            $user = $this->User_model->get_by_name($this->user['name']);
-            if (empty($user)) {
-                $user = array(
-                    'name' => $this->user['name'],
-                    'nick' => $user_data->userInfo->name,
-                    'size' => $this->config->item('limit', 'upload')
-                );              
-
-                $user['id'] = $this->User_model->insert_entry($user);
-                $user['auth'] = 0;
-                $user['used'] = 0;
-                $user['lastgroup'] = 0;
-
-                //$this->szmongo->user_add($user);
-            } else {
-                $user['nick'] = $user_data->userInfo->name;
-            }
-
-            $this->user = $user;
-            $this->user['real_size'] = $this->user['size'];
-            $this->user['real_used'] = $this->user['used'];
-            $this->user['per'] = round($this->user['real_used']/$this->user['real_size']*100,2);
-            $this->user['size'] = format_size($this->user['real_size']);
-            $this->user['used'] = format_size($this->user['real_used']);
+            $cookie = array(
+                'name'   => 'skey',
+                'value'  => $this->skey,
+                'expire' => '86500',
+                'domain' => '.71xiaoxue.com',
+                'path'   => '/',
+                'secure' => TRUE
+            );
+            $this->input->set_cookie($cookie);
 
         } else {
             if ($this->uri->uri_string() === '') {
                 phpCAS::forceAuthentication();
             } else {
                 $this->user['id'] = '0';
+                $this->skey = FALSE;
                 return;
             }
         }
+    }
+
+    protected function check_skey($skey)
+    {
+        // 使用encodeKey获取用户信息
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://mapp.71xiaoxue.com/components/getUserInfo.htm');
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'encodeKey='.$skey);
+        $user_str = curl_exec($ch);
+        curl_close($ch);
+
+        $user_data = json_decode(strstr($user_str, '{'));
+        if (is_null($user_data)) {
+            log_message('ERROR', '获取用户信息返回字符串解析错误：'.$user_str.' KEY:'.$this->skey);
+            $this->show_error('用户数据服务器不可用，请联系系统管理员', 503);
+        }
+
+        if ($user_data->resultMsg != 'ok' || !$user_data->success) {
+            log_message('ERROR', '用户信息无法正确验证：'.$user_str.' KEY:'.$this->skey);
+            $this->show_error('无法验证用户信息，请联系系统管理员', 403);
+        }
+
+        return $user_data;
+    }
+
+    protected function verify_user($user_data)
+    {
+        // 检查用户是否已经登录
+        $this->user['name'] = $user_data->userInfo->loginName;
+        $this->load->model('User_model');
+        $user = $this->User_model->get_by_name($this->user['name']);
+        if (empty($user)) {
+            $user = array(
+                'name' => $this->user['name'],
+                'nick' => $user_data->userInfo->name,
+                'size' => $this->config->item('limit', 'upload')
+            );
+
+            $user['id'] = $this->User_model->insert_entry($user);
+            $user['auth'] = 0;
+            $user['used'] = 0;
+            $user['lastgroup'] = 0;
+        } else {
+            $user['nick'] = $user_data->userInfo->name;
+        }
+
+        $this->user = $user;
+        $this->user['real_size'] = $this->user['size'];
+        $this->user['real_used'] = $this->user['used'];
+        $this->user['per'] = round($this->user['real_used']/$this->user['real_size']*100,2);
+        $this->user['size'] = format_size($this->user['real_size']);
+        $this->user['used'] = format_size($this->user['real_used']);
     }
 
     protected function set_group()
@@ -137,20 +158,12 @@ class SZone_Controller extends CI_Controller {
     {
         $result = array(
             'code' => $code,
-            'msg' => $this->lang->line($msg),
+            'msg' => $msg,
+            'data' => $data,
             'elapsed_time' => '{elapsed_time}',
             'memory_usage' => '{memory_usage}',
-            'data' => $data,
             'profiler' => '{profiler}'
         );
-        if ($code != '200')
-        {
-            log_message('error', $result['msg']);
-        }
-        else
-        {
-
-        }
         $this->output
             ->set_content_type('application/json')
             ->set_output($result);
@@ -166,8 +179,20 @@ class SZone_Controller extends CI_Controller {
             'code' => $code,
             'msg' => $msg
         );
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Content-type: application/json');
         echo json_encode($result);
         exit;
+    }
+
+    protected function show_error($msg, $code)
+    {
+        if ($this->uri->uri_string() === '') {
+            show_error($msg, $code);
+        } else {
+            $this->json_error($msg, $code);
+        }
     }
 
 }
