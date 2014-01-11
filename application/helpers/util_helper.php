0e7ff40dce27bb05a75d16172f2bad83d0d85419
katans <myhorde@vip.qq.com>
2014-01-11 09:14:45 +0800

    Update util_helper.php

--- application/helpers/util_helper.php
+++ application/helpers/util_helper.php
@@ -1,38 +1,4 @@
 <?php
-if ( ! function_exists('get_file_size')){
-	function get_file_size($size){
-		if($size > 1000000){
-			$size = round($used/1000000,2);
-			$size .='GB';
-		}elseif($size>1000){
-			$size = round($size/1000,2);
-			$size .='MB';
-		}else{
-			$size = (int) $size.'kb';
-		}
-		return $size;
-	}
-}
-
-if ( ! function_exists('get_file_type')){
-	function get_file_type($type){
-		switch($type){
-			case 1:
-				return '图片';
-			case 2:
-				return '文档';
-			case 3:
-				return '音乐';
-			case 4:
-				return '视频';
-			case 5:
-				return '应用';
-			case 6:
-				return '压缩包';
-		}
-		return '未知类型';
-	}
-}
 
 if(! function_exists('get_page_status')){
 	function get_page_status($np,$n,$al){
@@ -64,8 +30,8 @@ if(! function_exists('get_page_status')){
 
 if(! function_exists('get_file_type')){
 	function get_file_type($type){
-		$ret;
-		switch($item['type']){
+		$ret = '未知类型';
+		switch($type){
 			case 0:
 				$ret = '全部类型';
 				break;
@@ -88,6 +54,7 @@ if(! function_exists('get_file_type')){
 				$ret = '压缩包';
 				break;
 		}
+		return $ret;
 	}
 }
 
